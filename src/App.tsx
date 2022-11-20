c653f652617bb9bd321a3950cf9ac6aa5db23327
Magnus Rand <magnus.rand@gmail.com>
2022-11-20 20:22:53 +0100

    add groundwork for navigation menu #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import { LandigPage } from "./LandingPage";
+import { LandigPage } from "./pages/LandingPage";
 
 import "./App.css";
+import { MenuBar } from "./components/MenuBar";
+import { Routes, Route, useLocation } from "react-router-dom";
+
+export type Page = "/" | "/gaveonsker" | "/kontakt-oss" | "program";
 
 function App() {
+  const currentPath = useLocation();
   return (
     <div className="App">
-      <LandigPage />
+      <Routes>
+        <Route path="/" element={<LandigPage />} />
+      </Routes>
+      <MenuBar activeElement={currentPath.pathname} />
     </div>
   );
 }
