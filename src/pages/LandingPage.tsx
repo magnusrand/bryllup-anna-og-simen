88ac33448f9a0bdcab0e91ddff4d6e6c99738220
Magnus Rand <magnus.rand@gmail.com>
2022-12-16 19:21:11 +0100

    add stawrtwork for frontpage and adaptive colors

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 
+import "./LandingPage.css";
+
 export const LandigPage = () => {
   return (
-    <div>
-      <h1>Her blir det forside</h1>
+    <div className="landing-page__wrapper">
+      <div className="landing-page__header-box page-header">
+        <div>Anna</div>
+        <div className="landing-page__header__ampersand">
+          <div className="landing-page__header__line" />&
+          <div className="landing-page__header__line" />
+        </div>
+        <div>Simen</div>
+      </div>
+      <div className="landing-page__content">
+        Vi gifter oss! Og vi håper du vil være med oss og feire denne spesielle
+        dagen ♥️
+      </div>
     </div>
   );
 };
