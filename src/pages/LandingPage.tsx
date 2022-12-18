64c4a44934bde62e21c2e2170ae1e3d5b23983cb
Magnus Rand <magnus.rand@gmail.com>
2022-12-18 21:34:03 +0100

    Add text to landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -7,17 +7,44 @@ export const LandigPage = () => {
     <div className="landing-page__wrapper">
       <div className="landing-page__header-box page-header">
         <div>Anna</div>
-        <div className="landing-page__header__ampersand">
-          <div className="landing-page__header__line" />&
-          <div className="landing-page__header__line" />
-        </div>
-        <div>Simen</div>
-        <div className="landing-page__header__date"> — 10 . 6 . 23 —</div>
+        <div className="landing-page__header__ampersand">og</div>
+        <div style={{ marginTop: "1rem" }}>Simen</div>
+        <div className="landing-page__header__date"> — 10. juni 2023 —</div>
       </div>
-      <div className="landing-page__content">
-        Vi gifter oss!
-        <br />
-        Og vi håper du vil være med oss og feire denne spesielle dagen ♥️
+      <div className="landing-page__content normal-text">
+        <h2 className="section-heading">Vi gifter oss!</h2>
+        <p>
+          Vi har den store gleden av å invitere venner og familie til
+          bryllupsfest 10. juni 2023 på Hamar!
+        </p>
+        <p>
+          Etter 12 år som kjærester er det på tide med en ring på fingeren, og
+          vi gleder oss masse til å dele den store dagen med dere! Vi har laget
+          et program for helgen og samlet litt nyttig informasjon som dere kan
+          finne i menyen på bunnen.
+        </p>
+        <p>
+          Vi ser frem til en helg fylt med alle vi er så glade i og en skikkelig
+          feiring av kjærligheten!!
+        </p>
+        <p>
+          Dersom dere har noen spørsmål eller innspill, ta enten kontakt med oss
+          eller toastmastere. Kontaktinformasjon ligger i menyen. Svar utbedes
+          1. april til en av oss – både for fredag og lørdag. Gi oss også
+          beskjed dersom det er noen forbehold som må tas i forhold til
+          matservering.
+        </p>
+        <p>
+          Siden vil bli oppdatert fortløpende frem mot bryllupsdagen. Håper vi
+          sees i juni ♥️
+        </p>
+        <p>
+          Mange klemmer fra{" "}
+          {/* <br />
+          <span style={{ fontFamily: "Cedarville Cursive", fontSize: "4rem" }}> */}
+          Anna og Simen
+          {/* </span> */}
+        </p>
       </div>
     </div>
   );
