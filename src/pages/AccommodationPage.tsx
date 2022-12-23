11f8e72f7a3b08bf8e3c453242373e12e6dfcc5c
Magnus Rand <magnus.rand@gmail.com>
2022-12-23 18:14:59 +0100

    update color palette

--- src/pages/AccommodationPage.tsx
+++ src/pages/AccommodationPage.tsx
@@ -5,18 +5,19 @@ import "./AccommodationPage.css";
 
 export const AccomodationPage = () => {
   return (
-    <div className="accomodation__content normal-text">
+    <div className="accommodation__content normal-text">
       <Header>Overnatting</Header>
       <p>
-        For de av dere som er i behov av overnatting er det flere gode muligheter i nærheten av både hjemmet vårt og kirken.
+        For de av dere som er i behov av overnatting er det flere gode
+        muligheter i nærheten av både hjemmet vårt og kirken.
       </p>
       <p>
-        VI har vært i kontakt med Scandic Hamar og allerede bestilt noen rom til rabattert pris. Si ifra dersom du eller dere ønsker å bo på Scandic for helgen, så kan vi forsøke å få tak i flere rom til denne prisen.
+        VI har vært i kontakt med Scandic Hamar og allerede bestilt noen rom til
+        rabattert pris. Si ifra dersom du eller dere ønsker å bo på Scandic for
+        helgen, så kan vi forsøke å få tak i flere rom til denne prisen.
       </p>
-      <p>
-        Andre alternativer for helgen er Victoria Hotel og Astoria Hotel.
-      </p>
-      <div className="accomodation__map">
+      <p>Andre alternativer for helgen er Victoria Hotel og Astoria Hotel.</p>
+      <div className="accommodation__map">
         <Wrapper apiKey={process.env.REACT_APP_MAPS_API_KEY as string}>
           <GoogleMap center={{ lat: 60.795904, lng: 11.086711 }} zoom={16} />
         </Wrapper>
