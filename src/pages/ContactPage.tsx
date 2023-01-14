a7216a1d870e82c9f363bff8d34e2f58aef460aa
Adrian Meidell Fiorito <adrian@sparkel.io>
2023-01-14 16:12:04 +0100

    Update contact and giftlist content

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -5,9 +5,18 @@ export const ContactPage = () => {
   return (
     <div className="contact__content">
       <Header>Kontaktinfo</Header>
-      <h2 className="section-heading">Kommer snart!</h2>
       <div className="normal-text">
-        <p>Det kommer relevant kontaktinfo her snart</p>
+        <p>
+          Vi kan kontaktes på telefon hva enn det måtte være! <br />
+          Anna <a href="tel:+4794037959"> 94037959</a> <br />
+          Simen <a href="tel:+4797114979"> 97114979</a>
+        </p>
+        <p>
+          Det vil også komme kontaktinformasjon til toastmaster nærmere
+          bryllupet. Dersom det er noen spørsmål om lokale eller lignende kan
+          arrangør, Kristin og Steven, kontaktes på kristin@skafferiethansen.net
+        </p>
+        <p>Håper vi sees i juni!!!</p>
       </div>
     </div>
   );
--- src/pages/GiftListPage.tsx
+++ src/pages/GiftListPage.tsx
@@ -1,4 +1,3 @@
-import { Link } from "../components/Link";
 import { Header } from "../components/Menu/Header";
 import "./GiftListPage.css";
 
@@ -6,16 +5,17 @@ export const GiftListPage = () => {
   return (
     <div className="gaveonsker__content">
       <Header>Gaveønsker</Header>
-      <h2 className="gaveonsker__description responsive-normal-text">
-        lenker til gavelister
-      </h2>
-      <Link href="https://www.kitchn.no/">Kitch'n</Link>
-      <Link href="https://jernbanemuseet.no/billettpriser/">
-        Jernbanemuseet
-      </Link>
-      <Link href="https://www2.hm.com/no_no/customer-service/gavekort/gavekort.html">
-        H&M gavekort
-      </Link>
+      <div className="normal-text">
+        <p>
+          Vårt største ønske er at så mange som mulig av dere vil komme å feire
+          dagen med oss!
+        </p>
+        <p>
+          Vi har allerede blitt spurt om gaveønsker fra noen av dere og har
+          derfor opprettet gaveliste på nett. Link til dette vil komme
+          etterhvert!
+        </p>
+      </div>
     </div>
   );
 };
