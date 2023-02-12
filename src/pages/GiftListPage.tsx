996f3eaabeb0025d11752962e74cdf724c04d259
Adrian Meidell Fiorito <adrian@sparkel.io>
2023-02-12 22:31:37 +0100

    Update giftlist page with actual giftlists

--- src/pages/GiftListPage.tsx
+++ src/pages/GiftListPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "../components/Link";
 import { Header } from "../components/Menu/Header";
 import "./GiftListPage.css";
 
@@ -12,8 +13,16 @@ export const GiftListPage = () => {
         </p>
         <p>
           Vi har allerede blitt spurt om gaveønsker fra noen av dere og har
-          derfor opprettet gaveliste på nett. Link til dette vil komme
-          etterhvert!
+          derfor opprettet gaveliste på nett.
+          <Link href="https://www.tilbords.no/onskeliste/109585">Tilbords</Link>
+          <Link href="https://www.illumsbolighus.no/on/demandware.store/Sites-illums_bolighus_no-Site/nb_NO/GiftRegistry-ShowOthers?id=c805ee672979b3e79bae40ce83">
+            Illums Bolighus
+          </Link>
+        </p>
+        <p>
+          Like kjært som gaver fra ønskelisten er et bidrag til bryllupsreise.
+          Vi har opprettet en reisekonto med kontonummer 1440 30 68811, og
+          gleder oss til en fantastisk Italiatur!
         </p>
       </div>
     </div>
